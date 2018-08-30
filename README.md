fe2ac879e49d9c52594b99eb54aa0ee3b4840af3
Manaday Mavani <manaday.mavani@investis.com>
2018-08-30 11:29:13 +0100

    Fixes map-stream writable error

--- README.md
+++ README.md
@@ -8,9 +8,12 @@ The file at `src/js/frame-mngr.js` contains error and hence should log an error 
 
 Remove/delete all code from the same file `src/js/frame-mngr.js` to check if everything works when there's no error. If you empty out the file content or delete the file itself. Things should work normally.
 
+![Gulp JSHint in Action](https://raw.githubusercontent.com/ManadayM/gulp-jshint-example/master/gulp-jshint-inaction.gif)
+
 ## Useful resources:
 - http://dev.topheman.com/gulp-fail-run-sequence-with-a-correct-exit-code/
 - https://github.com/spalger/gulp-jshint#custom-reporters
 - https://github.com/spalger/gulp-jshint/blob/master/src/reporters/fail.js
 - https://github.com/sindresorhus/jshint-stylish/blob/master/index.js
 - http://jshint.com/docs/reporters/
+- In cases when map-stream (here JSHint Fail Reporter) was being called more than once then I was getting `Error: map stream is not writable` error. This thread was helpful https://github.com/spalger/gulp-jshint/issues/50#issue-35343385
--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,27 @@ const jshint = require('gulp-jshint');
 const stylish = require('jshint-stylish');
 const map = require('map-stream');
 
-//
-const exitOnJSLintingError = map(function (file, cb) {
-  // return directly if it has success true
-  if (file.jshint.success) {
-    return cb(null, file);
-  }
-
-  // scan for each result object to detect if any error object has got actual Error?
-  file.jshint.results.forEach(result => {
-    // if code starts with `E` it's an error
-    if (result.error && result.error.code[0] === 'E') {
-      console.error('JSHint has caught error.');
-      // exit the process immediately
-      process.exit(1);
+const jsHintFailReporter = () => {
+  return map((file, cb) => {
+    // return directly if it has success true
+    if (file.jshint.success) {
+      return cb(null, file);
     }
-  });
 
-  // all good.. move further.
-  cb(null, file);
-});
+    // scan for each result object to detect if any error object has got actual Error?
+    file.jshint.results.forEach(result => {
+      // if code starts with `E` it's an error
+      if (result.error && result.error.code[0] === 'E') {
+        console.error('JSHint has caught error.');
+        // exit the process immediately
+        process.exit(1);
+      }
+    });
+
+    // all good.. move further.
+    cb(null, file);
+  });
+};
 
 // JavaScript task
 gulp.task('js', () => {
@@ -32,7 +33,7 @@ gulp.task('js', () => {
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
     //.pipe(jshint.reporter('fail'))
-    .pipe(exitOnJSLintingError)
+    .pipe(jsHintFailReporter())
     .pipe(uglify())
     .pipe(gulp.dest('dist/js/'));
 });
